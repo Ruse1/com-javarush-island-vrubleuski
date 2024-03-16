c02e641fe59942e046cbb08504943ca8454d3fc8
RuslanVrubleuski <6628762@mail.ru>
2024-03-16 15:00:10 +0300

    Create ConfigAnimals.java, rename ConfigurationIsland-> ConfigIsland, redact ConfigIsland

--- src/main/java/com/javarush/island/vrubleuski/entity/area/Island.java
+++ src/main/java/com/javarush/island/vrubleuski/entity/area/Island.java
@@ -3,8 +3,8 @@ package com.javarush.island.vrubleuski.entity.area;
 public class Island {
     private final Location[][] locations;
 
-    public Island(Location[][] locations) {
-        this.locations = locations;
+    public Island(int width, int height) {
+        this.locations = new Location[width][height];
     }
 
     public Location[][] getLocations() {
