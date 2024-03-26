f9eba38c9d1a11556b56a5440ab305480b2db97c
RuslanVrubleuski <6628762@mail.ru>
2024-03-26 19:55:10 +0300

    Method move is written

--- src/main/java/com/javarush/island/vrubleuski/App.java
+++ src/main/java/com/javarush/island/vrubleuski/App.java
@@ -1,16 +1,16 @@
 package com.javarush.island.vrubleuski;
 
 import com.javarush.island.vrubleuski.configuration.ConfigIsland;
-import com.javarush.island.vrubleuski.entity.animal.*;
 import com.javarush.island.vrubleuski.entity.area.Island;
-import com.javarush.island.vrubleuski.entity.area.Location;
 
 public class App {
     public final static Island island = new Island(ConfigIsland.WIDTH, ConfigIsland.HEIGHT);
 
     public static void main(String[] args) {
         //МОЙ ПРОЕКТ ЕЩЁ В РАБОТЕ!!!
-        // ВСЕ МЕТОДА ЖИВОТНЫХ РАСПИСАННЫ
+        //ВСЕ МЕТОДЫ ЖИВОТНЫХ НАПИСАНЫ
+        //ОСТАЛОСЬ СОБРАТЬ ПРИЛОЖЕНИЕ БЕЗ МНОГОПОТОЧНОСТИ
+        //ОСЛОСЬ СОБРАТЬ ПРИЛОЖЕНИЕ С МНОГОПОТОЧНОСТЬЮ
 
     }
 
